d6b7e33aa3a9fec3307f081396c885686bd6c843
Christy Inman <christyinman@ymail.com>
2019-06-01 19:55:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(d_array)
+  d_array.each_with_index { |item, index| }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(call_array)
+  call_array.collect do |x|
+    x.capitalize << "!"
+  end  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(c_array)
+  c_array.any? do |word|
+    word.length > 4
+  end  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |x|
+    x.include?(cheese_types)
+  end  
 end
